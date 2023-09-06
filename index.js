142ceeaff51519032a1b36612bbc2ba80e5f04b7
Riokurniawan-id <riosayang911@gmail.com>
2023-09-06 21:38:01 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// juragan angkot
+
+const angkot1 = (data) => {
+  let angkot = 1;
+
+  while (angkot <= data) {
+    console.log("angkot no." + angkot + " beroprasi dengan baik");
+    angkot++;
+  }
+};
+angkot1(); // tulis disini jumlah angkot
+
+const angkot2 = (data) => {
+  let angkot = data;
+};
